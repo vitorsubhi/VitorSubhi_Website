daaa4bc3f7e92acbd0546bbd71bc2880a31e7cd4
vitorsubhi <vitorsubhi@gmail.com>
2018-10-15 13:42:46 -0300

    First Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,14 +59,11 @@
 
 				$bg
 					.removeClass('fixed')
-					.css('transform', 'matrix(1,0,0,1,0,0)');
 
 				$window
 					.on('scroll._parallax', function() {
 
 						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
-
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
 
 					});
 
